1b4e45bee4b6b66e85866989c40143f8392a50ed
SeungjunNA <nsjjsng1@gmail.com>
2024-01-08 19:52:24 +0900

    Initial commit

---
+++ OneDrive/바탕 화면/project/src/test/java/com/cloudtrack/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudtrack.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
