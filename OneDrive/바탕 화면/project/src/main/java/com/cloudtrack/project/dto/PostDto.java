188ada43a13551d3021eedb2195873e985e2a5db
SeungjunNA <nsjjsng1@gmail.com>
2024-01-09 18:10:04 +0900

    게시글작성 기능/조회 기능 구현

--- OneDrive/바탕 화면/project/src/main/java/com/cloudtrack/project/dto/PostDto.java
+++ OneDrive/바탕 화면/project/src/main/java/com/cloudtrack/project/dto/PostDto.java
@@ -1,13 +1,26 @@
 package com.cloudtrack.project.dto;
 
 import com.cloudtrack.project.domain.Post;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class PostDto {
     private long id;
     private String title;
     private String content;
     private String country;
+    private String editPassword;
+//    public PostDto(String title, String content, String country, String editPassword){
+//        this.title = title;
+//        this.content = content;
+//        this.country = country;
+//        this.editPassword = editPassword;
+//    }
     public Post toEntity(){
-        return new Post(title, content, country);
+        return new Post(title, content, country, editPassword);
     }
 }
