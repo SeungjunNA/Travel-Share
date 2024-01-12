a5618f43721b56efc8be7ae0030b58bc36c2dae5
SeungjunNA <nsjjsng1@gmail.com>
2024-01-12 22:00:20 +0900

    feat: 댓글 작성하여 데이터베이스에 저장

--- OneDrive/바탕 화면/project/src/main/java/com/cloudtrack/project/Entity/Comment.java
+++ OneDrive/바탕 화면/project/src/main/java/com/cloudtrack/project/Entity/Comment.java
@@ -2,18 +2,22 @@ package com.cloudtrack.project.Entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter @Setter
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long commentId;
     private String content;
-    @ManyToOne
-    private String postId;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "postId")
+    private Post post;
 
     public Comment(String content){
         this.content = content;
--- OneDrive/바탕 화면/project/src/main/java/com/cloudtrack/project/controller/CommentController.java
+++ OneDrive/바탕 화면/project/src/main/java/com/cloudtrack/project/controller/CommentController.java
@@ -6,15 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CommentController {
     @Autowired
     private CommentService commentService;
     @PostMapping("/comment")
-    public String createComment(@ModelAttribute("commentDto") CommentDto commentDto){
-        commentService.createComment(commentDto);
-
-        return "redirect:/travel/detail-post";
+    public String createComment(@ModelAttribute("commentDto") CommentDto commentDto, @RequestParam("postId") long postId){
+        commentService.createComment(commentDto, postId);
+        return "redirect:/travel/detail-post/"+postId;
     }
 }
